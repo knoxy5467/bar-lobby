921416bf4d1b7952f1ea68fb44b1045017cec179
Michael Knox <knoxy5467@gmail.com>
2023-04-14 00:11:31 -0400

    started work on generalized queue element for use in future

---
+++ src/renderer/components/misc/queue.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex-box">
+        <h1 class="queue-name">{{ props.queueName }}</h1>
+        <div class = "queue-checks-container">
+
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+    queueName: string;
+    queue: string;
+}>();
+</script>
